5568a2a64d68a23ee0714bada06c0e92cb52c6e0
Michael-Tyler <miketylerabc@gmail.com>
2020-11-29 19:02:49 -0600

    nothing

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -23,7 +23,7 @@ class App extends React.Component {
         if (!this.state.errorMessage && this.state.lat){
             return <SeasonDisplay lat={this.state.lat} />
         }
-        return <Spinner message={'Please accept location request'}/>
+        return <Spinner message={'Please accept location request'}/> 
     }
 
     render() {
