6705b2201fc1aa7b4994382c217cb6b8d912d655
Michael-Tyler <miketylerabc@gmail.com>
2021-01-24 11:31:50 -0600

    Finished uo translate for now, refactor after I finish streams

---
+++ translate/src/components/Button.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext'
+
+class Button extends React.Component {
+  submitRender(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
+  renderButton(color){
+    return (
+    <button className={`ui button ${color}`}>
+      <LanguageContext.Consumer>
+        {(value)=> this.submitRender(value)}
+      </LanguageContext.Consumer>
+  </button>
+    )
+  }
+  
+  render() {
+
+    return (
+      <ColorContext.Consumer>
+      { (color) =>
+        this.renderButton(color)
+      }
+      </ColorContext.Consumer>
+    )
+  }
+};
+
+export default Button;
