18463cff80bd4df1816d477ba6cffd6c47603432
Michael-Tyler <miketylerabc@gmail.com>
2021-01-23 18:57:58 -0600

    commit to move to another module

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Route } from "react-router-dom";
+import Header from './Header';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
-  return <div>App</div>
+  return (
+  <div className="ui container">
+    <Header />
+      <Route path="/" exact component={StreamList} />
+      <Route path="/streams/new" exact component={StreamCreate} />
+      <Route path="/streams/edit" exact component={StreamEdit} />
+      <Route path="/streams/delete" exact component={StreamDelete} />
+      <Route path="/streams/show" exact component={StreamShow} />
+  </div>
+  )
 }
 
 export default App;
---
+++ streams/client/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+  <div className="ui secondary pointing menu">
+    <Link to="/" className="item">
+      Streamer
+    </Link>
+    <div className="right menu">
+    <Link to="/" className="item">
+      All Streams
+    </Link>
+    <Link to="/" className="item">
+      Log in with Google
+    </Link>
+    </div>
+  </div>
+  )
+};
+
+export default Header;
